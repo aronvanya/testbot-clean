bb5313f8d8ede70cb8bbe09c295ef3868e21d647
aronvanya <aronvanya@gmail.com>
2025-01-20 21:27:13 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-WEBHOOK_URL = "https://testbot-clean.vercel.app/webhook"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
     if data:
-        # Обработка полученных данных
         print(f"Received data: {data}")
     return jsonify({"message": "Webhook received!"}), 200
 
