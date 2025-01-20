04935474dc0523e6558a5266c2cfa66c2fc0432b
aronvanya <aronvanya@gmail.com>
2025-01-20 21:44:55 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,8 @@
-from flask import Flask, request, jsonify
+# index.py
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.get_json()
-    if data:
-        print(f"Received data: {data}")
-    return jsonify({"message": "Webhook received!"}), 200
-
-@app.route('/')
-def index():
-    return "Server is running", 200
+@app.route("/")
+def home():
+    return "Hello, Vercel!"
