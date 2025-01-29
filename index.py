331290841fc3d0eef68ce3ef96395d80f08ffa0f
aronvanya <aronvanya@gmail.com>
2025-01-29 15:09:44 +0300

    Update index.py

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ import requests
 import instaloader
 import subprocess
 import re
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -68,7 +69,7 @@ def send_reels_video(chat_id, reels_url, user_name):
 
             if not is_valid_video(video_content):
                 send_video_as_document(chat_id, video_content, user_name, reason="размер > 20MB или длительность > 60 секунд")
-            elif not is_valid_aspect_ratio():
+            elif not is_valid_aspect_ratio(video_content):
                 send_video_as_document(chat_id, video_content, user_name, reason="нестандартное соотношение сторон")
             else:
                 send_video_as_stream(chat_id, video_content, user_name)
@@ -85,20 +86,22 @@ def is_valid_video(video_content):
     video_size_mb = len(video_content) / (1024 * 1024)
     return video_size_mb <= 20  # Проверяем размер (до 20MB)
 
-def is_valid_aspect_ratio():
+def is_valid_aspect_ratio(video_content):
     try:
-        result = subprocess.run([
-            "ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0", "temp_video.mp4"
-        ], capture_output=True, text=True)
+        process = subprocess.run([
+            "ffprobe", "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0"],
+            input=video_content,
+            capture_output=True,
+            text=True)
         
-        match = re.search(r"(\d+),(\d+)", result.stdout)
+        match = re.search(r"(\d+),(\d+)", process.stdout)
         if match:
             width, height = map(int, match.groups())
             aspect_ratio = width / height
             return 0.56 <= aspect_ratio <= 1.91  # Соотношение сторон от 9:16 до 16:9
     except Exception as e:
         print(f"Ошибка проверки соотношения сторон: {e}")
-    return True  # Если проверка не удалась, считаем, что соотношение корректное
+    return True
 
 def send_video_as_stream(chat_id, video_content, user_name):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendVideo"
